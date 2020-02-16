14da1ed76a1edbe09bf095eadb1b293336dbc9b9
davidcorrado <davidcorrado@gmail.com>
2020-02-16 16:37:43 -0600

    ios

---
+++ iOS/Location Permission/Location Permissions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Location Permissions
+//
+//  Created by David Corrado on 2/16/20.
+//  Copyright © 2020 David Corrado. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
