4cf818f599d9abf5d9ec7486f3eb74926c1d33d6
davidcorrado <davidcorrado@gmail.com>
2020-02-16 23:40:07 -0600

    ios seems to work properly

--- iOS/Location Permissions/Location Permissions/ViewController.swift
+++ iOS/Location Permissions/Location Permissions/ViewController.swift
@@ -7,14 +7,64 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var label: UILabel!
+    let locationManager = CLLocationManager()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        locationManager.delegate = self
+        if(CLLocationManager.locationServicesEnabled()) {
+            let locStatus = CLLocationManager.authorizationStatus()
+            switch locStatus {
+                case .notDetermined:
+                    locationManager.requestAlwaysAuthorization()
+                    //locationManager.requestWhenInUseAuthorization()
+                    //locationManager.requestLocation()
+                    return
+                case .denied, .restricted:
+                    print("Permission Denied")
+                    label.text = "Permission Denied"
+                    createSettingsAlertController()
+                    return
+                case .authorizedAlways, .authorizedWhenInUse:
+                    print("Permission Authorized")
+                    label.text = "Permission Authorized"
+                    break
+                @unknown default:
+                    fatalError()
+            }
+        } else {
+            print("Location Services Denied")
+            label.text = "Location Services Denied"
+            createSettingsAlertController()
+        }
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if (status == CLAuthorizationStatus.denied) {
+            print("Permission Denied")
+            label.text = "Permission Denied"
+        } else if (status == CLAuthorizationStatus.authorizedAlways) {
+            print("Permission Authorized")
+            label.text = "Permission Authorized"
+        }
+    }
+    
+    func getAppName() -> String{
+        return Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as? String ?? ""
+    }
+    
+    func createSettingsAlertController() {
+      let alertController = UIAlertController(title: "Turn On Location Services to allow \""+getAppName()+"\" to determine your location", message: nil, preferredStyle: .alert)
+      let cancelAction = UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel, handler: nil)
+      let settingsAction = UIAlertAction(title: NSLocalizedString("Settings", comment: ""), style: .default) { (UIAlertAction) in
+        UIApplication.shared.openURL(URL(string: UIApplication.openSettingsURLString)!)
+      }
+      alertController.addAction(cancelAction)
+      alertController.addAction(settingsAction)
+      self.present(alertController, animated: true, completion: nil)
+    }
 }
 
