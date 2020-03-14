cba8f29e4a5225da219e421eedc570c962c293f0
davidcorrado <davidcorrado@gmail.com>
2020-03-14 15:22:44 -0500

    Testing Android.

--- Flutter/location_services_permissions/example/lib/main.dart
+++ Flutter/location_services_permissions/example/lib/main.dart
@@ -12,23 +12,28 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<LocationServicesPermissionsResult>(
-        stream:
-            LocationServicesPermissions.pollLocationServicesAndPermissions(),
-        builder: (BuildContext context,
-            AsyncSnapshot<LocationServicesPermissionsResult> snapshot) {
-          print("LocationServices" + snapshot.connectionState.toString());
-          print("LocationServices" + snapshot.data.toString());
-          if (!snapshot.hasData) {
-            return Center(child: CircularProgressIndicator());
-          }
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: StreamBuilder<LocationServicesPermissionsResult>(
+            stream: LocationServicesPermissions
+                .pollLocationServicesAndPermissions(),
+            builder: (BuildContext context,
+                AsyncSnapshot<LocationServicesPermissionsResult> snapshot) {
+              print("LocationServices" + snapshot.connectionState.toString());
+              print("LocationServices" + snapshot.data.toString());
+              if (!snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
+              }
 
-          //if (snapshot.data == GeolocationStatus.denied) {
-          return Center(child: Text(snapshot.data.locationServicesPermissionsState.toString(),
-              textDirection: TextDirection.ltr));
-          //}
-
-          // return _buildListView();
-        });
+              return Center(
+                  child: Text(
+                      snapshot.data.locationServicesPermissionsState.toString(),
+                      textDirection: TextDirection.ltr));
+            }),
+      ),
+    );
   }
 }
